e9477d1b0437fe176daf7ca97787a58ebf8f380d
yeyunwen <1571374338@qq.com>
2024-06-18 23:46:08 +0800

    chore(create-api): 删除index.ts无用代码

--- packages/create-api/src/index.ts
+++ packages/create-api/src/index.ts
@@ -1,18 +1,6 @@
-import Handlebars from "handlebars";
 import axios from "axios";
-import type {
-  OpenAPI3,
-  OperationObject,
-  ParameterObject,
-  ResponsesObject,
-} from "openapi-typescript";
+import type { OpenAPI3 } from "openapi-typescript";
 import { ServiceGenerator } from "./serviceGenerator";
-
-type ReqMethod = typeof REQ_METHOD;
-const REQ_METHOD = {
-  get: "getRequst",
-  post: "postRequst",
-};
 
 const getApiJson = async (url: string) => {
   try {
@@ -22,50 +10,6 @@ const getApiJson = async (url: string) => {
     console.log("getApiJson error: ", error);
     process.exit(1);
   }
-};
-
-type ApiInfo = {
-  url: string;
-  method: keyof ReqMethod;
-};
-
-const generateParams = (parameters: ParameterObject[]) => {
-  if (!parameters) return;
-  console.log("Handlebars", Handlebars);
-
-  const template = Handlebars.compile(`
-    type ParamsType = {
-      {{#each parameters}}
-        /**
-         * {{ description }}
-         */
-        {{ name }}{{required : "" ? "?"}}: {{ schema.type }};
-      {{/each}}
-    }
-    `);
-  return template({ parameters });
-};
-
-const generateResponse = (responses: ResponsesObject) => {
-  if (!responses || !responses["200"]) return "any";
-};
-
-export const generateRequest = async (
-  pathItem: OperationObject,
-  info: ApiInfo,
-) => {
-  const { parameters, responses } = pathItem;
-  const { url, method } = info;
-  const paramsType = generateParams(parameters as ParameterObject[]);
-  const responseType = generateResponse(responses!);
-  return `
-type ParamsType = ${paramsType || "any"}
-type ResponseType = ${responseType}
-
-export const regionTree = (params: ParamsType) =>
-    ${
-      REQ_METHOD[method]
-    }<typeof params, IResponseType<ResponseType>>(${url}, params)`;
 };
 
 type ResFnOption = {
@@ -80,7 +24,3 @@ const genResFn = async ({ url }: ResFnOption) => {
 };
 
 export default genResFn;
-
-genResFn({
-  url: "http://127.0.0.1:4523/export/openapi/2?version=3.0",
-});
